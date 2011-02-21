39b4baed94e8a446434cef882c7134c6521ae703
Serj Alimsky <alimsky@gmail.com>
2011-02-21 12:28:07 +0300

    Selects repaired, radiobuttons applied, text types added

--- magicfill.js
+++ magicfill.js
@@ -1,109 +1,140 @@
 (function(){
 
 function valueGenerator(){
-//generate word function
-this.CONSONANTS=['b','c','d','f','g','h','j','k','l','m','n','p','r','s','t','v','w','x','z','ch','sh', 'fr','q'];
-this.VOWELS=['a','e','i','o','u','y', 'oo', 'ou', 'ae', 'ea'];
-this.generateWord=function(lngth, firstLetterLower){
+		//Some hardcoded data... TODO:Drop it to options.html
+		this.EMAIL=['mail'];
+		this.NUMBER=['numb', 'integer', 'price', 'size', 'val'];
+		this.EMAIL_HOSTING=['gmail.com','hotmail.com','yahoo.com','mail.ru'];
+		this.CONSONANTS=['b','c','d','f','g','h','j','k','l','m','n','p','r','s','t','v','w','x','z','ch','sh', 'fr','q'];
+		this.VOWELS=['a','e','i','o','u','y', 'oo', 'ou', 'ae', 'ea'];
 
-		//Define length of word
-    if(!lngth)
-        lngth=Math.floor(Math.random()*10)+3;
-    
-    var resultWord='';
-    var odd=true; //alternation of vowels and consonants
+		//generate word function
+		this.generateWord=function(lngth, firstLetterLower){
 
-		//Too obvious to comment anything here
-    while (resultWord.length < lngth){
-        newSymbol = odd?this.CONSONANTS[Math.floor(Math.random()*this.CONSONANTS.length)]:this.VOWELS[Math.floor(Math.random()*this.VOWELS.length)];
-        odd=!odd;
-        resultWord+=newSymbol;
-    }
+				//Define length of word
+				if(!lngth)
+						lngth=Math.floor(Math.random()*10)+3;
+				
+				var resultWord='';
+				var odd=true; //alternation of vowels and consonants
 
-		//Make uppercase first letter of generated word
-		if(!firstLetterLower)
-			resultWord = resultWord[0].toUpperCase() + resultWord.substring(1);
+				//Too obvious to comment anything here
+				while (resultWord.length < lngth){
+						newSymbol = odd?this.CONSONANTS[Math.floor(Math.random()*this.CONSONANTS.length)]:this.VOWELS[Math.floor(Math.random()*this.VOWELS.length)];
+						odd=!odd;
+						resultWord+=newSymbol;
+				}
 
-    return resultWord;
-}
+				//Make uppercase first letter of generated word
+				if(!firstLetterLower)
+					resultWord = resultWord[0].toUpperCase() + resultWord.substring(1);
 
-this.generatePhrase=function(lngth){
-		var resultPhrase='';
-    if(!lngth){
-        lngth=Math.floor(Math.random()*24)+5;
-    };
-		for (var word=0; word<=lngth; word++){
-			if (word==lngth)
-				resultPhrase=resultPhrase + this.generateWord(null, true) + '.';
-			else if(word==1)
-				resultPhrase=this.generateWord()+' ';
-			else
-				resultPhrase=resultPhrase + this.generateWord(null, true) + ' ';
+				return resultWord;
 		}
-		return resultPhrase;
-};
 
-this.EMAIL_HOSTING=['gmail.com','hotmail.com','yahoo.com','mail.ru'];
-this.generateEmail=function(){
-		return this.generateWord()+'@'+this.EMAIL_HOSTING[Math.floor(Math.random()*this.EMAIL_HOSTING.length)];
-}
+		//generate whole phrases, inserting big letter in beggining and the dot at the end. Writing comments is so boring...
+		this.generatePhrase=function(lngth){
+				var resultPhrase='';
+				if(!lngth){
+						lngth=Math.floor(Math.random()*24)+5;
+				};
+				for (var word=0; word<=lngth; word++){
+					if (word==lngth)
+						resultPhrase=resultPhrase + this.generateWord(null, true) + '.';
+					else if(word==1)
+						resultPhrase=this.generateWord()+' ';
+					else
+						resultPhrase=resultPhrase + this.generateWord(null, true) + ' ';
+				}
+				return resultPhrase;
+		};
 
-this.selectValue=function(select_control){
-	select_control.selectedIndex = Math.floor(Math.random()*(select_control.options.length+1));
-};
-this.checkValue=function(checkbox){
-	checkbox.checked = (Math.random()>0.5)?'checked':'';
-};
-
-this.EMAIL=['mail'];
-this.NUMBER=['numb', 'integer', 'price', 'size', 'val'];
-this.isAnyEqual=function(st, ar){
-	for (el in ar){
-		if(st.indexOf(ar[el])>0){
-			return 1;
+		//generate email with random word and random selected email_hosting
+		this.generateEmail=function(){
+				return this.generateWord(null, true)+'@'+this.EMAIL_HOSTING[Math.floor(Math.random()*this.EMAIL_HOSTING.length)];
 		}
-	}
-	return -1;
-}
 
-this.valueBasedOnType=function(inp){
-	q=inp.id+' '+inp.name+' '+inp.className;
-	if(isAnyEqual(q, this.EMAIL))
-		return this.generateEmail();
-	else if(isAnyEqual(q, this.NUMBER)
-		return Math.random()*10000;
-	else 
-		return this.generateWord();
-}
+		//Select random option in dropdown... but it works weird. TODO:Look into it.
+		this.selectValue=function(select_control){
+				rVal = Math.floor(Math.random()*(select_control.options.length));
+				console.log(rVal);
+ 				select_control.selectedIndex = rVal;
+		};
+
+		//check randomly
+		this.checkValue=function(checkbox){
+			checkbox.checked = (Math.random()>0.5)?'checked':'';
+		};
+
+		//check one of radios
+		this.radioValue=function(radio){
+			var radios=[];
+			var pretendents=document.getElementsByName(radio.name);
+
+			//Select only true RADIO buttons :)
+			for(var i in pretendents) {
+				if(pretendents[i].type=="radio")  //malo li
+					radios.push(pretendents[i]);
+			}
+
+			//Do the magic!
+			radios[Math.floor(Math.random()*radios.length)].checked=true;
+
+		}
+
+		//Is any entries of the string ST equal to any lements of Array AR
+		this.isAnyEqual=function(st, ar){
+			for (elm in ar){
+				if(st.indexOf(ar[elm])>0){
+					return true;
+				}
+			}
+			return false;
+		}
+
+		//Return random value based on id, class and name of input
+		this.valueBasedOnType=function(inp){
+			q=inp.id+' '+inp.name+' '+inp.className;
+			if(this.isAnyEqual(q, this.EMAIL))
+				return this.generateEmail();
+			else if(this.isAnyEqual(q, this.NUMBER))
+				return (Math.random()*10000);
+			else 
+				return this.generateWord();
+		}
+
+		this.scanTheDomAndMakeSomeMagic=function(){
+				//Find all the inputs, and operate with them
+				el=document.getElementsByTagName('input');
+				for(var i in el) {
+					if(el[i].type=="text")
+						el[i].value=this.valueBasedOnType(el[i]);
+					else if(el[i].type=="password")
+						el[i].value="123123"; // TODO: unhardcode
+					else if(el[i].type=="checkbox")
+						this.checkValue(el[i]);
+					else if(el[i].type=="radio")
+						this.radioValue(el[i]);
+				}
+
+				//Find all the textareas and insert phrases in it.
+				el=document.getElementsByTagName('textarea');
+				for(var i=0; i<el.length; i++) {
+					el[i].value=this.generatePhrase();
+				}
+
+				//Select something in all selects
+				el=document.getElementsByTagName('select');
+				for(var i=0; i<el.length; i++) {
+					this.selectValue(el[i]);
+				}
+				return 'All right!';
+		}
 }
 
 if(!window.vG) 
 	window.vG=new valueGenerator();
 
-
-
-//Find all the inputs, and operate with them
-el=document.getElementsByTagName('input');
-for(var i=0; i<el.length; i++) {
-	if(el(i).type=="text")
-//		el(i).value=window.vG.generateWord();
-		el(i).value=vG.valueBasedOnType(el(i));
-	if(el(i).type=="pass")
-		el(i).value="123123";
-	if(el(i).type=="checkbox")
-		window.vG.checkValue(el(i));
-}
-
-//Find all the textareas and insert phrases in it.
-el=document.getElementsByTagName('textarea');
-for(var i=0; i<el.length; i++) {
-	el(i).value=window.vG.generatePhrase();
-}
-
-//Select something in all selects
-el=document.getElementsByTagName('select');
-for(var i=0; i<el.length; i++) {
-	window.vG.selectValue(el(i));
-}
+window.vG.scanTheDomAndMakeSomeMagic();
 
 })();
